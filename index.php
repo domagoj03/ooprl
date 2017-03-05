06f9db7664a5a44689a968603c4b861fb8bfa086
Domagoj <domagoj.blazonija@gmail.com>
2017-03-05 19:09:47 +0100

    DB class

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
 <?php 
 
+require_once 'core/init.php';
+
+$user = DB::getInstance()->get('users', ['username', '=', 'domagoj']);
+
+
+if (!$user->count()) {
+    echo "No user";
+} else {
+    echo "OK";
+}

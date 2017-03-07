ce1391cf28ab584ef7778f3060b3fd28b591cefe
Domagoj <domagoj.blazonija@gmail.com>
2017-03-07 22:57:38 +0100

    Register functionality

--- register.php
+++ register.php
@@ -30,8 +30,25 @@ if (Input::exists()) {
         ]);
 
         if ($validate->passed()) {
-            Session::flash('success', 'You registered successfully');
-            header('Location: index.php');
+            $user = new User();
+
+            $salt = Hash::salt(32);
+
+            try {
+                $user->create([
+                    'username' => Input::get('username'),
+                    'password' => Hash::make(Input::get('password'), $salt),
+                    'salt' => $salt,
+                    'name' => Input::get('name'),
+                    'joined' => date('Y-m-d H:i:s'),
+                    'group' => 1,
+                    ]);
+
+                Session::flash('home', 'You have been registered and can now log in!');
+                header('Location: index.php');
+            } catch(Exception $e) {
+                die($e->getMessage());
+            }
         } else {
             foreach ($validate->errors() as $error) {
                 echo "$error <br>";

7b63ecf8e20dd21067c006a8daaa97cdc409a199
Domagoj <domagoj.blazonija@gmail.com>
2017-03-07 19:44:42 +0100

    Validation and Input classes, form validation

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
 <?php 
 require_once 'core/init.php';
 
-$user = DB::getInstance()->update('users', 2, [
-    'password' => 'newpastest',
-    'name' => 'domic',
-    ]);
--- register.php
+++ register.php
@@ -1,3 +1,64 @@
 <?php 
-
 require_once 'core/init.php';
+
+if (Input::exists()) {
+    $validate = new Validate();
+    $validation = $validate->check($_POST, 
+    [
+        'username' => [
+            'required' => true;
+            'min' => 2,
+            'max' => 20,
+            'unique' => 'users'
+            ],
+        'password' => [
+            'required' => true,
+            'min' => 6,
+
+        ],
+        'password_again' => [
+            'required' => true,
+            'matches' => 'password',
+
+        ],
+        'name' => [
+            'required' => true,
+            'min' => 2,
+            'max' => 50, 
+        ],
+    ]);
+    
+    
+
+};
+?>
+
+<form action="" method="post">
+    <div class="field">
+        <label for="username">
+            Username
+        </label>
+        <input type="text" name="username" id="username" value="" autocomplete="off">
+    </div>
+    <div class="field">
+        <label for="password">
+            Choose a password
+        </label>
+        <input type="password" name="password" id="password">
+    </div>
+    <div class="field">
+        <label for="password_again">
+            Choose a password
+        </label>
+        <input type="password" name="password_again" id="password_again">
+    </div>
+    <div class="field">
+        <label for="name">
+            Name
+        </label>
+    </div>
+
+    <div class="field">
+        <input type="submit" value="Register">
+    </div>
+</form>
